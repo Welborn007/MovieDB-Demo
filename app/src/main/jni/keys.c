d6241af6f8dc01830ba93e93bff4d8898b58dd1d
Welborn Machado <machadowelborn@gmail.com>
2019-03-23 00:48:03 +0530

    FinalDemo

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -2,7 +2,7 @@
 JNIEXPORT jstring JNICALL
 Java_com_sample_mvvmarchitecture_Utilities_Constants_getAPIKey(JNIEnv *env, jobject instance) {
 
- return (*env)->  NewStringUTF(env, "02e6471fb33bd2c190c22b6a0f61d8e5");
+ return (*env)->  NewStringUTF(env, "Enter Your API Key Here");
 }
 
 JNIEXPORT jstring JNICALL
